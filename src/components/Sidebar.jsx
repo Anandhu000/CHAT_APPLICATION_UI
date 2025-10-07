5ec0c1183c72cca4857437236c966c87154b62fa
Anandhu krixx...!! <bksatheesh695@gmail.com>
2025-10-07 19:43:59 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,1 +1,23 @@
+import React from "react";
+
+const Sidebar = ({ contacts, onSelectChat }) => {
+  return (
+    <div className="w-64 bg-gray-900 text-white p-4 rounded-xl">
+      <h2 className="text-xl font-semibold mb-4">Chats</h2>
+      <ul>
+        {contacts.map((c, i) => (
+          <li
+            key={i}
+            className="p-2 hover:bg-gray-700 rounded cursor-pointer"
+            onClick={() => onSelectChat(c)}
+          >
+            {c.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
 
