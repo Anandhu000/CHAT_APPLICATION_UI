4dd86872f0b6b4aff4bfe18228d0bb840c5a69bc
Anandhu krixx...!! <bksatheesh695@gmail.com>
2025-10-07 19:30:18 +0530

    Update useChat.js

--- src/hooks/useChat.js
+++ src/hooks/useChat.js
@@ -1,1 +1,25 @@
-dddd
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:5000");
+
+export const useChat = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on("receive_message", (message) => {
+      setMessages((prev) => [...prev, message]);
+    });
+
+    return () => {
+      socket.off("receive_message");
+    };
+  }, []);
+
+  const sendMessage = (message) => {
+    socket.emit("send_message", message);
+  };
+
+  return { messages, sendMessage };
+};
+
